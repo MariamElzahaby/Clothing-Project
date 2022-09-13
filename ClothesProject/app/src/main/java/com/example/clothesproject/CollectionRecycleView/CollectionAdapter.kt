90deeb51c09176f7ce1e8f186b43d2909c67c736
DESKTOP-NR2RQON\Memo <elzahabymariam@gmail.com>
2022-09-13 13:15:47 +0200

    update

--- ClothesProject/app/src/main/java/com/example/clothesproject/CollectionRecycleView/CollectionAdapter.kt
+++ ClothesProject/app/src/main/java/com/example/clothesproject/CollectionRecycleView/CollectionAdapter.kt
@@ -9,26 +9,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.clothesproject.DummyData.Data
 import com.example.clothesproject.R
 
+
 class CollectionAdapter (
-    private val dataList: List<Data>
-        ): RecyclerView.Adapter<CollectionAdapter.Holder>() {
+    private val dataList: List<Data>,
+): RecyclerView.Adapter<CollectionAdapter.Holder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionAdapter.Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.product, parent, false)
-        return Holder(view)
-    }
 
-    override fun onBindViewHolder(holder: CollectionAdapter.Holder, position: Int) {
-           holder.bind(dataList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return dataList.size
-    }
-
-    class Holder(view: View): RecyclerView.ViewHolder(view){
+    inner class Holder(view: View): RecyclerView.ViewHolder(view){
         var tv_pName: TextView
         var tv_price: TextView
+        lateinit var data: Data
 
         init {
             tv_pName = view.findViewById(R.id.tv_productName)
@@ -36,8 +26,38 @@ class CollectionAdapter (
         }
 
         fun bind(data: Data) {
+            this.data = data
             tv_pName.text = data.name
             tv_price.text = data.price
+
+            itemView.setOnClickListener {
+                onItemClickListener?.also {
+                    it(data)
+                }
+            }
         }
+
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionAdapter.Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: CollectionAdapter.Holder, position: Int) {
+        holder.bind(dataList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+
+
+    private var onItemClickListener: ((Data) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Data) -> Unit) {
+        onItemClickListener = listener
+    }
+
 }
--- ClothesProject/app/src/main/java/com/example/clothesproject/MainActivity.kt
+++ ClothesProject/app/src/main/java/com/example/clothesproject/MainActivity.kt
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        lifecycleScope.launch {
+            delay(1500)
+            navigateToHome()
+        }
+    }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-           }, 1500
-        )
+    private fun navigateToHome(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
